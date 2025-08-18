137b034377dd2503d5702febc4ea6341f73af4cb
Om Mehta <64518918+ommehta4920@users.noreply.github.com>
2025-08-18 18:13:31 +0530

    app.py file updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 app = Flask(__name__)
 # Load the model
-regmodel = pickle.load(open("regmodel.pkl", "rb"))  
+regmodel = pickle.load(open("regmodel.pkl", "rb"))
 scaler = pickle.load(open("scaling.pkl", "rb"))
 
 
@@ -30,7 +30,7 @@ def predict():
     final_input = scaler.transform(np.array(data).reshape(1, -1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
-    return render_template('home.html', prediction_text="The predicted house price is ${}".format(round(output * 100000))) 
+    return render_template('home.html', prediction_text="The predicted house price is ${}".format(round(output * 100000)))
 
 
 if __name__ == "__main__":
