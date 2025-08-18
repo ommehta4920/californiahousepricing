a83b3d6a8898ccbf2260aaea2516d8869b36bb6c
Om Mehta <64518918+ommehta4920@users.noreply.github.com>
2025-08-18 18:00:44 +0530

    unwanter imports removed

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, app, jsonify, render_template
 import numpy as np
-import pandas as pd
 
 app = Flask(__name__)
 ## Load the model
