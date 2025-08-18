a2a44118178d8a326d36969415213e5e215a31df
Om Mehta <64518918+ommehta4920@users.noreply.github.com>
2025-08-18 18:02:33 +0530

    some errors fixed

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def predict():
     final_input = scaler.transform(np.array(data).reshape(1, -1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
-    return render_template('home.html', prediction_text = "The predicted house price is ${}".format(round(output * 100000))) 
+    return render_template('home.html', prediction_text="The predicted house price is ${}".format(round(output * 100000))) 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
